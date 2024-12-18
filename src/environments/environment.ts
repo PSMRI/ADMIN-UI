38d834ef6bce891d6028d0524c11554c8c960498
NKOKKILIGADDA87 <nagendra.kokiligadda@wipro.com>
2024-12-18 17:51:12 +0530

    remove Enckey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -29,7 +29,7 @@ const COMMON_API = 'https://amritwprdev.piramalswasthya.org/';
 const adminBaseUrl = `${ADMIN_API}adminapi-v3.0.0/`;
 const superadminBaseURL = `${ADMIN_API}adminapi-v3.0.0/`;
 const commonBaseURL = `${COMMON_API}commonapi-v3.0.0/`;
-const sessionStorageEncKey = 'asdfghjkgfdfghj';
+const sessionStorageEncKey = '<>';
 
 export const environment = {
   production: false,
